456084192bb0b300536aee02b208a8b4e3f662ac
dipplestix <chrism315@gmail.com>
2023-11-16 01:23:34 -0500

    Speed up

--- marketsim/simulator/sampled_arrival_simulator.py
+++ marketsim/simulator/sampled_arrival_simulator.py
@@ -4,9 +4,12 @@ from marketsim.market.market import Market
 from marketsim.fundamental.mean_reverting import GaussianMeanReverting
 from marketsim.fundamental.lazy_mean_reverting import LazyGaussianMeanReverting
 from marketsim.agent.zero_information_agent import ZIAgent
+import torch.distributions as dist
+import torch
+from collections import defaultdict
 
 
-class Simulator:
+class SimulatorSampledArrival:
     def __init__(self, num_agents: int, sim_time: int, num_assets: int = 1, lam=0.1, mean=100, r=.6, shock_var=10):
         self.num_agents = num_agents
         self.num_assets = num_assets
@@ -14,14 +17,22 @@ class Simulator:
         self.lam = lam
         self.time = 0
 
+        self.arrivals = defaultdict(list)
+        self.arrivals_sampled = 10000
+        self.arrival_times = sample_arrivals(lam, self.arrivals_sampled)
+        self.arrival_index = 0
+
         self.markets = []
         for _ in range(num_assets):
-            fundamental = GaussianMeanReverting(mean=mean, final_time=sim_time, r=r, shock_var=shock_var)
-            # fundamental = LazyGaussianMeanReverting(mean=mean, final_time=sim_time, r=r, shock_var=shock_var)
+            # fundamental = GaussianMeanReverting(mean=mean, final_time=sim_time, r=r, shock_var=shock_var)
+            fundamental = LazyGaussianMeanReverting(mean=mean, final_time=sim_time, r=r, shock_var=shock_var)
             self.markets.append(Market(fundamental=fundamental, time_steps=sim_time))
 
         self.agents = {}
         for agent_id in range(num_agents):
+            self.arrivals[self.arrival_times[self.arrival_index].item()].append(agent_id)
+            self.arrival_index += 1
+
             self.agents[agent_id] = (
                 ZIAgent(
                     agent_id=agent_id,
@@ -33,24 +44,29 @@ class Simulator:
                 ))
 
     def step(self):
-        # print(f'It is time step {self.time}')
+        agents = self.arrivals[self.time]
         if self.time < self.sim_time:
             for market in self.markets:
-                for agent_id in self.agents:
-                    if random.random() <= self.lam:
-                        agent = self.agents[agent_id]
-                        market.withdraw_all(agent_id)
-                        side = random.choice([BUY, SELL])
-                        order = agent.take_action(side)
-                        # print(f'Agent {agent.agent_id} is entering the market and makes order {order}')
-                        market.add_order(order)
+                for agent_id in agents:
+                    agent = self.agents[agent_id]
+                    market.withdraw_all(agent_id)
+                    side = random.choice([BUY, SELL])
+                    order = agent.take_action(side)
+                    market.add_order(order)
+
+                    if self.arrival_index == self.arrivals_sampled:
+                        self.arrival_times = sample_arrivals(self.lam, self.arrivals_sampled)
+                        self.arrival_index = 0
+                    self.arrivals[self.arrival_times[self.arrival_index].item() + 1 + self.time].append(agent_id)
+                    self.arrival_index += 1
+
                 new_orders = market.step()
                 for matched_order in new_orders:
                     agent_id = matched_order.order.agent_id
                     quantity = matched_order.order.order_type*matched_order.order.quantity
                     cash = -matched_order.price*matched_order.order.quantity*matched_order.order.order_type
                     self.agents[agent_id].update_position(quantity, cash)
-            self.time += 1
+
         else:
             self.end_sim()
 
@@ -63,5 +79,15 @@ class Simulator:
         # print(f'At the end of the simulation we get {values}')
 
     def run(self):
-        for t in range(self.sim_time + 1):
-            self.step()
+        counter = 0
+        for t in range(self.sim_time):
+            if self.arrivals[t]:
+                self.step()
+                counter += 1
+            self.time += 1
+        self.step()
+
+
+def sample_arrivals(p, num_samples):
+    geometric_dist = dist.Geometric(torch.tensor([p]))
+    return geometric_dist.sample((num_samples,)).squeeze()  # Returns a tensor of 1000 sampled time steps

c860805348ef103c0d45a8ef92d5caaf7b34c434
Rishith Seelam <rseelam@umich.edu>
2024-06-05 13:42:44 -0400

    added noisy obs extended ZI agent

---
+++ marketsim/agent/obs_noise_extended_ZI.py
@@ -0,0 +1,65 @@
+import random
+from marketsim.agent.agent import Agent
+from marketsim.market.market import Market
+from marketsim.fourheap.order import Order
+from marketsim.private_values.private_values import PrivateValues
+from marketsim.fourheap.constants import BUY, SELL
+from typing import List
+
+
+class ZIAgent(Agent):
+    def __init__(self, agent_id: int, market: Market, q_max: int, offset: float, eta: float, shade: List, obs_var: int):
+        self.agent_id = agent_id
+        self.market = market
+        self.pv = PrivateValues(q_max)
+        self.position = 0
+        self.offset = offset
+        self.eta = eta
+        self.shade = shade
+        self.obs_var = obs_var
+        self.obs_std = obs_var ** 0.5
+        self.cash = 0
+
+
+    def get_id(self) -> int:
+        return self.agent_id
+
+    def estimate_fundamental(self):
+        mean, r, T = self.market.get_info()
+        t = self.market.get_time()
+        noisy_val = self.market.get_fundamental_value() + random.gauss(0, self.obs_std)
+
+        rho = (1-r)**(T-t)
+
+        estimate = (1-rho)*mean + rho*noisy_val
+        return estimate
+
+    def take_action(self, side):
+        t = self.market.get_time()
+        estimate = self.estimate_fundamental()
+        spread = self.shade[1] - self.shade[0]
+        price = estimate + side*spread*random.random() + self.shade[0]
+
+
+        return Order(
+            price=price,
+            quantity=1,
+            agent_id=self.get_id(),
+            time=t,
+            order_type=side,
+            order_id=random.randint(1, 10000000)
+                     )
+
+    def update_position(self, q, p):
+        self.position += q
+        self.cash += p
+
+    def __str__(self):
+        return f'ZI{self.agent_id}'
+
+    def get_pos_value(self) -> float:
+        return self.pv.value_at_position(self.position)
+
+    def reset(self):
+        self.position = 0
+        self.cash = 0

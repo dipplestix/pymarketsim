d3c6e5a7d10581268686ab13fc271fbaf9421382
wangyzh <wangyzh@umich.edu>
2024-06-28 17:40:23 -0400

    "add sys to import from parent class"

--- marketsim/MM/simMM_example.py
+++ marketsim/MM/simMM_example.py
@@ -1,6 +1,18 @@
 import os
 import sys
 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
 from marketsim.MM.simMM import SimulatorSampledArrival_MM
 import numpy as np
 from absl import app
